a13c4c6fb846726eda3ea27ba714291bf2967c27
Ana Muedas <muedasanacristina@gmail.com>
2023-04-08 10:13:10 -0500

    commit inicial

---
+++ Models/Bebida.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentosCSHARP.Models
+{
+    class Bebida
+    {
+        public string Nombre { get; set; }
+
+        public int Cantidad { get; set; } 
+
+        public Bebida (string Nombre, int Cantidad)
+        {
+            this.Nombre = Nombre;
+            this.Cantidad = Cantidad;
+        }
+
+        public void Beber(int cantbebida)
+        {
+            this.Cantidad -= cantbebida;
+        }       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using FundamentosCSHARP.Models;
+using System.Collections.Generic;
+namespace FundamentosCSHARP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var persona = new { nombre = "Ana", apellido = "Muedas", edad = 27 };
+            Console.WriteLine(persona.apellido);
+
+            Console.WriteLine("Hello World!");
+            Bebida bebida = new Bebida("Coca Cola", 1000);
+            bebida.Beber(500);
+            Console.WriteLine(bebida.Cantidad);
+            Cerveza cerveza = new Cerveza();
+            Console.WriteLine(cerveza.Cantidad);
+
+            int[] numeros = new int[5] { 1,2,3,4,5 };
+            int numero = numeros[0];
+            Console.WriteLine(numero);
+
+            for(int i=0; i<numeros.Length; i++)
+            {
+                Console.WriteLine(numeros[i]);
+            }
+            foreach(var numerito in numeros)
+            {
+                Console.WriteLine(numerito);
+            }
+
+            List<int> lista = new List<int>() { 1, 2, 3, 4 };
+            lista.Add(5);
+            lista.Add(6);
+            Console.WriteLine(lista);
+
+            foreach (var valor in lista)
+            {
+                Console.WriteLine(valor);
+            }
+
+        }
+    }
+}
